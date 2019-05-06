3971a8ab248cce484a1a71193cbea96e3e759a10
lukas-heiligenbrunner <ultimategameingcookie@gmail.com>
2019-05-06 18:49:38 +0200

    reformated output of main

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,20 @@
 
 int main(int argc, char *argv[]) {
 
-    if(argc > 1){
+    if (argc > 1) {
         std::string firstarg(argv[1]);
-        if (firstarg == "-h" || firstarg == "--help"){
-            std::cout << "help page: " << std::endl << "[-h] [--help]  print this help page" << std::endl << "[-v] [--version] print the software version" <<std::endl << "[no argument] normal ip check and refresh" << std::endl;
-        }else if(firstarg == "-v" || firstarg == "--version"){
+        if (firstarg == "-h" || firstarg == "--help") {
+            std::cout << "help page: " << std::endl << "[-h] [--help]  print this help page" << std::endl
+                      << "[-v] [--version] print the software version" << std::endl
+                      << "[no argument] normal ip check and refresh" << std::endl;
+        } else if (firstarg == "-v" || firstarg == "--version") {
             std::cout << "Version 1.0" << std::endl;
-        }else{
+        } else {
             std::cout << "wrong arguments!  -h for help" << std::endl;
         }
-    } else{
+    } else {
         API api;
         Logger logger;
-
 
         std::string ip = api.request("https://api.ipify.org");
 
@@ -26,33 +27,33 @@ int main(int argc, char *argv[]) {
             //no internet connection
             logger.logToLogfile("[WARNING] no internet connection");
             std::cout << "[WARNING] no internet connection" << std::endl;
-
         } else {
             std::string oldip = logger.readip();
 
             if (oldip == ip) {
-                std::cout << "no change -- ip: " << ip << std::endl;
+                std::cout << "[INFO] no change -- ip: " << ip << std::endl;
                 logger.logToLogfile(" [INFO] no change -- ip: " + ip);
             } else {
                 logger.logToLogfile(" [INFO] ip changed! -- from :" + oldip + "to: " + ip);
-                std::cout << "ip changed! -- from :" << oldip << "to: " << ip << std::endl;
+                std::cout << "[INFO] ip changed! -- from :" << oldip << "to: " << ip << std::endl;
 
-                //dynu api key: 88vNpMfDhMM2YYDNfWR1DNYfRX9W6fYg
+                static std::string dynuapikey = "88vNpMfDhMM2YYDNfWR1DNYfRX9W6fYg";
 
                 static std::string domainid = "8506047"; //id of the dynu domain
+                static std::string domainname = "luki.dynu.net";
 
                 Hashmap<std::string, std::string> args;
-                args.add("name", "luki.dynu.net");
+                args.add("name", domainname);
                 args.add("ipv4Address", ip);
 
                 std::vector<std::string> headers;
                 headers.push_back("accept: application/json");
                 headers.push_back("User-Agent: Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)");
-                headers.push_back("API-Key: 88vNpMfDhMM2YYDNfWR1DNYfRX9W6fYg");
+                headers.push_back("API-Key: " + dynuapikey);
 
                 std::string dynurepl = api.request("https://api.dynu.com/v2/dns/" + domainid, true, args, headers);
 
-                std::cout << "[ DEBUG ] api reply:: " << dynurepl << std::endl;
+                std::cout << "[DEBUG] api reply:: " << dynurepl << std::endl;
 
                 if (dynurepl != "{\"statusCode\":200}") {
                     logger.logToLogfile(" [ERROR] failed to write ip to dynu api!");
