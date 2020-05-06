ca5e654e857bbdcbc2b62a9b869ad43dbaf0363e
lukas <lukas.heiligenbrunner@gmail.com>
2020-05-06 21:31:49 +0200

    optionally build tests or not added some realistic tests

--- tests/UnitTest.cpp
+++ tests/UnitTest.cpp
@@ -2,26 +2,29 @@
 // Created by lukas on 06.05.20.
 //
 
+#include <FileLogger.h>
+#include <api/IPAPI.h>
+#include <climits>
 #include "gtest/gtest.h"
-#include "IPRefresher.h"
 
-TEST (SquareTest /*test suite name*/, LukisTest /*test name*/) {
-//EXPECT_EQ (9.0, (3.0*3.0)); // fail, test continues
-ASSERT_EQ (0.0, (0.0));     // success
-//ASSERT_EQ (9, (3)*(-3.0));  // fail, test interrupts
-//ASSERT_EQ (-9, (-3)*(-3.0));// not executed due to the previous assert
+/**
+ * Test if default ip is 0.0.0.0 when last ip file doesn't exist.
+ */
+TEST(ReadIp, testzeroIpIfNotExists) {
+    FileLogger logger;
+    std::string oldip = logger.readip();
+    ASSERT_EQ(oldip, "0.0.0.0");
 }
-
-TEST(Bla, test2){
-    ASSERT_EQ(5.0,5.0);
-}
-
-TEST(Bla, test5){
-    ASSERT_EQ(4.0,5.0);
-}
-
-TEST(IPRefresher, test1){
-    IPRefresher ipr;
-
-    ASSERT_EQ(5.0,5.0);
+/**
+ * Test if default ip is 0.0.0.0 when last ip file doesn't exist.
+ */
+TEST(IPAPI, testIpAPIcheckIPSyntax) {
+    IPAPI ipapi;
+    std::string ip = ipapi.getGlobalIp();
+    if (ip.find('.') == ULONG_MAX) {
+        // error when ip doesn't contain a .
+        ASSERT_TRUE(false);
+    } else {
+        ASSERT_TRUE(true);
+    }
 }
